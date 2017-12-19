6e9e7bd665bea0bc9452d03d14b541a4f9519418
“dihtyar” <bogdandihtyar@gmail.com>
2017-12-19 12:06:42 +0200

    'for'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+
+  users = [
+    {name: 'title f1'},
+    {name: 'title f2'},
+    {name: 'title f3'},
+    {name: 'title f4'},
+    {name: 'title f5'},
+    {name: 'title f6'}
+  ]
 
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./user.component.scss'],
 })
 
-export class UserComponent {}
+export class UserComponent {
+  @Input() user;
+}
